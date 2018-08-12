9c4add0217135dbdf08de2006e2f98fdb0945ab7
Madeorsk <madeorsk@protonmail.com>
2018-08-12 22:18:31 +0200

    Poll visualisation.
    Finished poll visualisation. TODO: Buttons now need to do things.

--- views/poll.php
+++ views/poll.php
@@ -1,6 +1,14 @@
 <h1 class="poll"><?= $poll->title ?></h1>
 <main>
-	<pre>
-		<?= var_dump($poll) ?>
-	</pre>
+  <form action="#" id="poll">
+    <?php foreach ($poll->options as $option): ?>
+    <div class="option">
+      <input type="radio" name="options" value="<?= $option->id ?>" id="option-<?= $option->id ?>" />
+      <label for="option-<?= $option->id ?>" class="check"></label>
+      <label for="option-<?= $option->id ?>"><?= $option->label ?></label>
+    </div>
+    <?php endforeach; ?>
+    <input type="submit" value="Vote" />
+  </form>
+  <a class="button margin">Jump to results</a>
 </main>
